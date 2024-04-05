aa7d4c1dbf3b02729aa601b98d838dca00659f7f
Vinay Selukar <vselukar2102@gmail.com>
2024-04-05 12:04:49 +0530

    Delete .env.template and update Accordion component

--- src/modules/products/components/product-tabs/accordion.tsx
+++ src/modules/products/components/product-tabs/accordion.tsx
@@ -19,20 +19,19 @@ type AccordionItemProps = AccordionPrimitive.AccordionItemProps & {
 
 type AccordionProps =
   | (AccordionPrimitive.AccordionSingleProps &
-      React.RefAttributes<HTMLDivElement>)
+    React.RefAttributes<HTMLDivElement>)
   | (AccordionPrimitive.AccordionMultipleProps &
-      React.RefAttributes<HTMLDivElement>)
+    React.RefAttributes<HTMLDivElement>)
 
-const Accordion: React.FC<AccordionProps> & {
+const Accordion: React.FC<any> & {
   Item: React.FC<AccordionItemProps>
 } = ({ children, ...props }) => {
   return (
-    /* @ts-expect-error */
     <AccordionPrimitive.Root {...props}>{children}</AccordionPrimitive.Root>
   )
 }
 
-const Item: React.FC<AccordionItemProps> = ({
+const Item: React.FC<any> = ({
   title,
   subtitle,
   description,
@@ -45,7 +44,6 @@ const Item: React.FC<AccordionItemProps> = ({
   ...props
 }) => {
   return (
-    /* @ts-expect-error */
     <AccordionPrimitive.Item
       {...props}
       className={clx(
@@ -54,14 +52,12 @@ const Item: React.FC<AccordionItemProps> = ({
         className
       )}
     >
-      {/* @ts-expect-error */}
       <AccordionPrimitive.Header className="px-1">
         <div className="flex flex-col">
           <div className="flex w-full items-center justify-between">
             <div className="flex items-center gap-4">
               <Text className="text-ui-fg-subtle text-sm">{title}</Text>
             </div>
-            {/* @ts-expect-error */}
             <AccordionPrimitive.Trigger>
               {customTrigger || <MorphingTrigger />}
             </AccordionPrimitive.Trigger>
@@ -73,7 +69,6 @@ const Item: React.FC<AccordionItemProps> = ({
           )}
         </div>
       </AccordionPrimitive.Header>
-      {/* @ts-expect-error */}
       <AccordionPrimitive.Content
         forceMount={forceMountContent}
         className={clx(
